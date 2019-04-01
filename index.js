f11197a636c54dc5117dd20257a4f05fe53062bf
Melissa Real <melissa.real@hotmail.com>
2019-04-01 06:40:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,16 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState;
   this.training = training;
 
-  
+  this.veto = function(){
+    console.log("No, I must disagree");
+  }
+  this.sayHello = function(){
+    console.log("Hello everybody!");
+  }
+  this.sayHello = function(){
+    console.log("Hello everybody!");
+  }
+  this.sayHello = function(){
+    console.log("Hello everybody!");
+  }
 }
